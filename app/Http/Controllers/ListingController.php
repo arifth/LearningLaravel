0c1a587284fcc489f33c815759070580b325f2ee
arifthalhah <arifthalhah@gmail.com>
2022-06-20 17:19:49 +0700

    adding naming convention to index and show route

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -9,7 +9,7 @@ class ListingController extends Controller
 {
     //show all listings
     public function index(){
-        return view('listings',[
+        return view('listings.index',[
             'heading' => "this is a heading",
             'listings' => Listing::all()
            ]);   
@@ -18,7 +18,7 @@ class ListingController extends Controller
 
     //show single listing
     public function show(Listing $listing){
-        return view('listing',[
+        return view('listing.show',[
             'listing' => $listing
         ]);
     }
